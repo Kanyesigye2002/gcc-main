753aa4a45d6c04978bdf9da38f261ec1d88fb309
Kanyesigye2002 <kanyeallan@gmail.com>
2021-07-03 17:26:30 +0300

    main6

--- src/Redux/Url.js
+++ src/Redux/Url.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export const Url = "http://localhost:8080";
+export const Url1 = "http://localhost:8080";
 export const Url2 = "http://localhost:8081";
-export const Url1 = "https://gcc-backend.herokuapp.com";
+export const Url = "https://gcc-backend.herokuapp.com";
