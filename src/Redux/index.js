c2e11ffa7f199d25d9677b731b0f017ace3533b6
Kanyesigye2002 <kanyeallan@gmail.com>
2021-07-04 17:41:50 +0300

    master3

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -2,21 +2,21 @@ import {applyMiddleware, createStore, compose} from 'redux'
 import rootReducer from "./Reducers";
 import thunk from 'redux-thunk'
 
-const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
+// const store = createStore(
+//     rootReducer,
+//     compose(
+//         applyMiddleware(thunk),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+// )
 
 store.subscribe(() => {
     saveState(store.getState())
 })
 
-// const store = createStore(
-//     rootReducer, applyMiddleware(thunk)
-// )
+const store = createStore(
+    rootReducer, applyMiddleware(thunk)
+)
 export default store
 
 // const loadState = () => {
