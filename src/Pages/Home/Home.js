c12ec557eedf133b60f853a4ec04dd70a5cbc373
allankanye <kanyeallan@gmail.com>
2021-08-05 14:21:55 +0300

    Commit app performance

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,7 +7,6 @@ import {useDispatch, useSelector} from "react-redux";
 import {NextService, Carousels, Iframe, Swipes} from '../../Components'
 import {fetchData} from '../../Redux/Actions'
 import Button from "@material-ui/core/Button";
-import {Zoom} from "react-reveal";
 
 function Home() {
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import store from "./Redux";
 import Theme from "./Theme";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 ReactDOM.render(
     <Provider store={store}>
-        <Theme/>
+        <React.Suspense fallback={
+            <CircularProgress color="primary"/>
+        }>
+            <Theme/>
+        </React.Suspense>
     </Provider>,
     document.getElementById('root')
 );
